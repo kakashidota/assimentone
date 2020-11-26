20a89a203fd3d234f66a0766efec2e5263ed01da
Robin Kamo <robin.kamo@live.se>
2020-11-26 14:35:12 +0100

    My first commit. added a bunch of stuff

---
+++ Assigmnet.py
@@ -0,0 +1,34 @@
+#string
+hej = "hello word!"
+print(hej)
+#float
+#math
+pi = 3.14
+
+radius = 5
+area = pi * radius * radius
+
+area = pi * radius ** 2
+
+print (area)
+
+div = 5 // 3
+print(div)
+
+weeks = round(365 / 7, 5)
+print(weeks)
+
+mystory = "I dont like cheese, but i like TOMATOS!"
+print(mystory * 3)
+
+
+name = "AleXanDrA"
+
+print(name.upper())
+print(name.lower())
+print(len(name))
+
+mening = "Lars är duktig på att koda, men inte så duktig på att laga mat"
+print(mening)
+print(mening.replace("Lars", "Matilda"))
+print(mening.split())
